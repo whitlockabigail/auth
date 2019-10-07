802a8a90e88fb9ade2605f8ada61947c723a3a90
whitlockabigail <55110985+whitlockabigail@users.noreply.github.com>
2019-10-07 00:25:04 -0600

    please

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Button from "@material-ui/core/Button";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
 import Drawer from "@material-ui/core/Drawer";
-import { Link, Route } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { auth } from "./firebase";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
